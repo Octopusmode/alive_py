49cba5c19a9768187c154628722b8f9001218b47
Octopusmode <osmininmaks52@gmail.com>
2020-05-21 03:57:28 +0300

    More then nothing

--- inc/creations.py
+++ inc/creations.py
@@ -1,6 +1,17 @@
+import random as rnd
 from typing import Optional, Union
 from dataclasses import dataclass
-from inc.etc import percent
+from inc.etc import percent, value_by_percent, ContainLevel as CL
+
+
+class Creature:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+        self.name = kwargs.pop('name')
+        self.age = kwargs.pop('age')
+        self.is_solid = kwargs.pop('is_solid')
+        self.is_bloody = kwargs.pop('bloody')
+
 
 @dataclass
 class Gender:
@@ -13,11 +24,63 @@ class Gender:
 @dataclass
 class Solid:
     is_solid: bool = False
+    _desc: str = 'A solid creature.'
 
     def set_solid(self):
         self.is_solid = True
 
+    def desc(self):
+        return self._desc
 
-class ObjectDescription(Gender, Solid):
+
+@dataclass
+class Bloody:
+    _desc: str = ''
+    is_bloody: bool = False
+    blood_min_per: int = rnd.randrange(5, 70, 1)
+    blood_vol_max: float = rnd.uniform(0.1, 1.0)
+    blood_vol_min: float = blood_vol_max - value_by_percent(blood_vol_max, blood_min_per)
+    blood_vol_cur: float = blood_vol_max
+    is_bleeding: bool = False
+    death_reason: bool = False
+
+    def set_bloody(self):
+        self.is_bloody = True
+
+    def set_bleeding(self, level):
+        self.is_bleeding = True
+        if self.is_bloody and self.is_bleeding:
+            self.blood_vol_cur -= level
+        else:
+            return 'Where is no effect!'
+
+    def blood_loss_death(self, name='creature'):
+        if self.is_bloody and self.blood_vol_cur < self.blood_vol_min:
+            self.death_reason = True
+            self._desc = 'The {0}, dyed by blood loss'.format(name)
+            return self.death_reason
+
+    @property
+    def condition(self):
+        blood_bal: float = percent((self.blood_vol_max - self.blood_vol_min), self.blood_vol_cur)
+        if blood_bal >= CL.PERFECT:
+            return 'The creature has no blood loss.'
+        elif blood_bal >= CL.SLIGHTLY:
+            return 'Slight blood loss.'
+        elif blood_bal >= CL.MINOR:
+            return 'Minor blood loss.'
+        elif blood_bal >= CL.MEDIUM:
+            return 'Medium blood loss.'
+        elif blood_bal >= CL.MAJOR:
+            return 'Major blood loss.'
+        elif blood_bal >= CL.CRITICAL:
+            return 'Critical blood loss!'
+        elif blood_bal >= CL.FATAL:
+            return 'Fatal blood loss!'
+        elif blood_bal == CL.EMPTY:
+            return 'The creature is completely drained of blood... huh'
+
+
+class ObjectDescription(Gender, Solid, Bloody):
     def __init__(self):
         super().__init__()
--- inc/etc.py
+++ inc/etc.py
@@ -1,5 +1,33 @@
 from typing import Union
+from enum import IntEnum, Enum
+
+
+class ContainLevel(IntEnum):
+    PERFECT: int = 98
+    SLIGHTLY: int = 90
+    MINOR: int = 80
+    MEDIUM: int = 60
+    MAJOR: int = 40
+    CRITICAL: int = 30
+    FATAL: int = 10
+    EMPTY: int = 0
+
+
+class ActionLevel(Enum):
+    HARMLESS: float = 0.1
+    WEAKEST: float = 0.2
+    WEAK: float = 0.3
+    MINOR: float = 0.4
+    AVERAGE: float = 0.5
+    MAJOR: float = 0.6
+    STRONG: float = 0.8
+    STRONGEST: float = 0.9
+    IRRESISTIBLE: float = 1.0
 
 
 def percent(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
     return x / y * 100
+
+
+def value_by_percent(x: Union[int, float], y: Union[int, float]) -> Union[int, float]:
+    return x / 100 * y
